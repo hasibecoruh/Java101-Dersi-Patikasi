d61c4a993ee8f0a41aad391758f0f6ac54ae2ea7
HasibeCoruh <coruhhasibe16@gmail.com>
2022-05-13 10:20:13 +0300

    Pratik - KDV Tutarını Hesaplayan Program

--- src/Temel_Kavramlar_ve_Degiskenler/Kdv_Tutari_Hesaplama.java
+++ src/Temel_Kavramlar_ve_Degiskenler/Kdv_Tutari_Hesaplama.java
@@ -1,2 +1,27 @@
-package Temel_Kavramlar_ve_Degiskenler;public class Kdv_Tutari_Hesaplama {
+package Temel_Kavramlar_ve_Degiskenler;
+
+import java.util.Scanner;
+
+public class Kdv_Tutari_Hesaplama {
+    public static void main(String[] args) {
+        double tutar,kdvTutar, kdvliTutar, kdv_orani;
+        boolean girilenTutar;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücret Tutarını Giriniz: ");
+        tutar = input.nextDouble();
+
+        girilenTutar = tutar > 1000;
+        kdv_orani = girilenTutar ? 0.08 : 0.18;
+
+        kdvTutar = tutar * kdv_orani;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("-----------------------------------");
+        System.out.println("KDV'siz Tutar: "+tutar);
+        System.out.println("KDV Oranı:" + kdv_orani);
+        System.out.println("KDV Tutarı:" +kdvTutar);
+        System.out.println("KDV'li Tutar: "+ kdvliTutar);
+
+    }
 }
